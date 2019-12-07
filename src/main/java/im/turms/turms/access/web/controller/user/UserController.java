4d179773db17af9aa5a271d5f2d3b0fbc7a418ac
JamesChenX <2511524953@qq.com>
2019-12-07 10:28:00 +0800

    Divide controllers more granularly and update swagger #125

--- src/main/java/im/turms/turms/access/web/controller/user/UserController.java
+++ src/main/java/im/turms/turms/access/web/controller/user/UserController.java
@@ -19,31 +19,18 @@ package im.turms.turms.access.web.controller.user;
 
 import im.turms.turms.access.web.util.ResponseFactory;
 import im.turms.turms.annotation.web.RequiredPermission;
-import im.turms.turms.cluster.TurmsClusterManager;
 import im.turms.turms.common.DateTimeUtil;
 import im.turms.turms.common.PageUtil;
 import im.turms.turms.common.TurmsStatusCode;
 import im.turms.turms.constant.AdminPermission;
-import im.turms.turms.constant.DeviceType;
 import im.turms.turms.constant.DivideBy;
-import im.turms.turms.constant.UserStatus;
 import im.turms.turms.exception.TurmsBusinessException;
-import im.turms.turms.pojo.bo.UserOnlineInfo;
-import im.turms.turms.pojo.domain.Group;
 import im.turms.turms.pojo.domain.User;
-import im.turms.turms.pojo.domain.UserLocation;
 import im.turms.turms.pojo.dto.*;
-import im.turms.turms.service.group.GroupMemberService;
-import im.turms.turms.service.group.GroupService;
 import im.turms.turms.service.message.MessageService;
 import im.turms.turms.service.user.UserService;
-import im.turms.turms.service.user.onlineuser.OnlineUserService;
-import im.turms.turms.service.user.onlineuser.UsersNearbyService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.socket.CloseStatus;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -53,25 +40,15 @@ import java.util.*;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    private final OnlineUserService onlineUserService;
-    private final UsersNearbyService usersNearbyService;
-    private final GroupService groupService;
-    private final GroupMemberService groupMemberService;
     private final MessageService messageService;
-    private final TurmsClusterManager turmsClusterManager;
     private final PageUtil pageUtil;
     private final DateTimeUtil dateTimeUtil;
 
-    public UserController(UserService userService, OnlineUserService onlineUserService, GroupService groupService, PageUtil pageUtil, UsersNearbyService usersNearbyService, MessageService messageService, DateTimeUtil dateTimeUtil, TurmsClusterManager turmsClusterManager, GroupMemberService groupMemberService) {
+    public UserController(UserService userService, PageUtil pageUtil, MessageService messageService, DateTimeUtil dateTimeUtil) {
         this.userService = userService;
-        this.onlineUserService = onlineUserService;
-        this.groupService = groupService;
         this.pageUtil = pageUtil;
-        this.usersNearbyService = usersNearbyService;
         this.messageService = messageService;
         this.dateTimeUtil = dateTimeUtil;
-        this.turmsClusterManager = turmsClusterManager;
-        this.groupMemberService = groupMemberService;
     }
 
     @GetMapping
@@ -267,111 +244,5 @@ public class UserController {
             }
         }
         return ResponseFactory.okIfTruthy(Flux.merge(counts).then(Mono.just(statistics)));
-    }
-
-    /**
-     * @param number this only works when userIds is null or empty
-     */
-    @GetMapping("/online-statuses")
-    @RequiredPermission(AdminPermission.USER_QUERY)
-    public Mono<ResponseEntity<ResponseDTO<Collection<UserOnlineInfo>>>> queryOnlineUsersStatus(
-            @RequestParam(required = false) Set<Long> userIds,
-            @RequestParam(defaultValue = "20") Integer number,
-            @RequestParam(defaultValue = "true") Boolean checkIfExists) {
-        if (userIds != null && !userIds.isEmpty()) {
-            List<Mono<UserOnlineInfo>> userOnlineInfoMonos = new ArrayList<>(userIds.size());
-            for (Long userId : userIds) {
-                Mono<UserOnlineInfo> userOnlineInfoMno = onlineUserService.queryUserOnlineInfo(userId);
-                userOnlineInfoMno = userOnlineInfoMno.flatMap(info -> {
-                    if (info.getUserStatus() == UserStatus.OFFLINE && checkIfExists) {
-                        return userService.userExists(userId)
-                                .flatMap(exists -> {
-                                    if (exists) {
-                                        return Mono.just(info);
-                                    } else {
-                                        return Mono.empty();
-                                    }
-                                });
-                    } else {
-                        return Mono.just(info);
-                    }
-                });
-                userOnlineInfoMonos.add(userOnlineInfoMno);
-            }
-            return ResponseFactory.okIfTruthy(Flux.merge(userOnlineInfoMonos));
-        } else {
-            if (number > turmsClusterManager.getTurmsProperties().getSecurity()
-                    .getMaxQueryOnlineUsersStatusPerRequest()) {
-                throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS);
-            }
-            Flux<UserOnlineInfo> userOnlineInfoFlux = onlineUserService.queryUserOnlineInfos(number);
-            return ResponseFactory.okIfTruthy(userOnlineInfoFlux);
-        }
-    }
-
-    @GetMapping("/online-statuses/count")
-    @RequiredPermission(AdminPermission.USER_QUERY)
-    public Mono<ResponseEntity<ResponseDTO<TotalDTO>>> countOnlineUsers() {
-        return ResponseFactory.total(onlineUserService.countOnlineUsers());
-    }
-
-    @PutMapping("/online-statuses")
-    @RequiredPermission(AdminPermission.USER_UPDATE)
-    public Mono<ResponseEntity<ResponseDTO<AcknowledgedDTO>>> updateUserOnlineStatus(
-            @RequestParam Long userId,
-            @RequestParam(required = false) Set<DeviceType> deviceTypes,
-            @RequestBody UpdateOnlineStatusDTO updateOnlineStatusDTO) {
-        Mono<Boolean> updated;
-        UserStatus onlineStatus = updateOnlineStatusDTO.getOnlineStatus();
-        if (onlineStatus == UserStatus.OFFLINE) {
-            if (deviceTypes != null) {
-                updated = onlineUserService.setUserDevicesOffline(userId, deviceTypes, CloseStatus.NORMAL);
-            } else {
-                updated = onlineUserService.setUserOffline(userId, CloseStatus.NORMAL);
-            }
-        } else {
-            updated = onlineUserService.updateOnlineUserStatus(userId, onlineStatus);
-        }
-        return ResponseFactory.acknowledged(updated);
-    }
-
-    @GetMapping("/users-nearby")
-    @RequiredPermission(AdminPermission.USER_QUERY)
-    public Mono<ResponseEntity<ResponseDTO<Collection<User>>>> queryUsersNearby(
-            @RequestParam Long userId,
-            @RequestParam(required = false) DeviceType deviceType,
-            @RequestParam(required = false) Integer maxPeopleNumber,
-            @RequestParam(required = false) Double maxDistance) {
-        Flux<User> usersNearby = usersNearbyService.queryUsersProfilesNearby(userId, deviceType, maxPeopleNumber, maxDistance);
-        return ResponseFactory.okIfTruthy(usersNearby);
-    }
-
-    @GetMapping("/locations")
-    @RequiredPermission(AdminPermission.USER_QUERY)
-    public ResponseEntity<ResponseDTO<Collection<UserLocation>>> queryUserLocations(@RequestParam Long userId) {
-        SortedSet<UserLocation> userLocations = onlineUserService.getUserLocations(userId);
-        return ResponseFactory.okIfTruthy(userLocations);
-    }
-
-    @GetMapping("/groups")
-    @RequiredPermission(AdminPermission.USER_QUERY)
-    public Mono<ResponseEntity<ResponseDTO<Collection<Group>>>> queryUserJoinedGroups(
-            @RequestParam Long userId,
-            @RequestParam(required = false) Integer size) {
-        size = pageUtil.getSize(size);
-        Flux<Group> groupsFlux = groupService.queryUserJoinedGroups(userId, 0, size);
-        return ResponseFactory.okIfTruthy(groupsFlux);
-    }
-
-    @GetMapping("/groups/page")
-    @RequiredPermission(AdminPermission.USER_QUERY)
-    public Mono<ResponseEntity<ResponseDTO<PaginationDTO<Group>>>> queryUserJoinedGroups(
-            @RequestParam Long userId,
-            @RequestParam(defaultValue = "0") Integer page,
-            @RequestParam(required = false) Integer size) {
-        size = pageUtil.getSize(size);
-        Mono<Long> count = groupMemberService.countUserJoinedGroupsIds(userId);
-        Flux<Group> groupsFlux = groupService.queryUserJoinedGroups(userId, page, size);
-        return ResponseFactory.page(count, groupsFlux);
     }
 }
