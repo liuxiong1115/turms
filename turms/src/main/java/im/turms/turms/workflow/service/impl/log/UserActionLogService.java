6207ba13c3610eb0c7d72869e2ec49354f2dc52a
JamesChenX <eurekajameschen@gmail.com>
2020-11-19 18:16:52 +0800

    Fix client requests are logged twice + log client requests in dev by default #453

--- turms/src/main/java/im/turms/turms/workflow/service/impl/log/UserActionLogService.java
+++ turms/src/main/java/im/turms/turms/workflow/service/impl/log/UserActionLogService.java
@@ -17,10 +17,15 @@
 
 package im.turms.turms.workflow.service.impl.log;
 
+import com.google.common.collect.Sets;
 import im.turms.common.constant.DeviceType;
 import im.turms.common.model.dto.request.TurmsRequest;
 import im.turms.server.common.cluster.node.Node;
 import im.turms.server.common.log4j.UserActivityLogging;
+import im.turms.server.common.property.constant.ActivityLoggingCategoryName;
+import im.turms.server.common.property.env.service.business.activity.ActivityLoggingProperties;
+import im.turms.server.common.property.env.service.business.activity.property.ActivityLoggingCategoryProperties;
+import im.turms.server.common.property.env.service.business.activity.property.ActivityLoggingRequestProperties;
 import im.turms.turms.bo.UserActionLog;
 import im.turms.turms.plugin.extension.handler.UserActionLogHandler;
 import im.turms.turms.plugin.manager.TurmsPluginManager;
@@ -28,9 +33,10 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static im.turms.common.model.dto.request.TurmsRequest.KindCase.*;
 
 /**
  * @author James Chen
@@ -40,12 +46,15 @@ public class UserActionLogService {
 
     private final Node node;
     private final TurmsPluginManager turmsPluginManager;
+    private final Map<TurmsRequest.KindCase, ActivityLoggingRequestProperties> includedRequests;
 
     public UserActionLogService(
             Node node,
             TurmsPluginManager turmsPluginManager) {
         this.node = node;
         this.turmsPluginManager = turmsPluginManager;
+        ActivityLoggingProperties loggingProperties = node.getSharedProperties().getService().getActivityLogging();
+        includedRequests = getIncludedRequests(loggingProperties);
     }
 
     public void tryLogAndTriggerLogHandlers(@NotNull Long userId, @NotNull DeviceType deviceType, @NotNull TurmsRequest request) {
@@ -59,7 +68,7 @@ public class UserActionLogService {
                     ? request.toString()
                     : null;
             userActionLog = new UserActionLog(userId, deviceType, new Date(), request.getKindCase().name(), actionDetails);
-            if (logUserAction) {
+            if (logUserAction && shouldLogClientRequest(request.getKindCase())) {
                 UserActivityLogging.log(userActionLog);
             }
             if (triggerHandlers) {
@@ -72,4 +81,215 @@ public class UserActionLogService {
         }
     }
 
+    private boolean shouldLogClientRequest(TurmsRequest.KindCase requestType) {
+        ActivityLoggingRequestProperties loggingRequest = includedRequests.get(requestType);
+        if (loggingRequest == null) {
+            return false;
+        }
+        float sampleRate = loggingRequest.getSampleRate();
+        if (sampleRate > 0) {
+            if (sampleRate < 1.0f) {
+                return ThreadLocalRandom.current().nextFloat() < sampleRate;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Map<TurmsRequest.KindCase, ActivityLoggingRequestProperties> getIncludedRequests(ActivityLoggingProperties properties) {
+        Map<TurmsRequest.KindCase, ActivityLoggingRequestProperties> result = new EnumMap<>(TurmsRequest.KindCase.class);
+
+        // 1. handle included categories
+        Set<ActivityLoggingCategoryProperties> includedCategories = properties.getIncludedCategories();
+        for (ActivityLoggingCategoryProperties includedCategory : includedCategories) {
+            Set<ActivityLoggingRequestProperties> loggingRequests = getRequestsFromCategory(includedCategory);
+            for (ActivityLoggingRequestProperties loggingRequest : loggingRequests) {
+                result.put(loggingRequest.getName(), loggingRequest);
+            }
+        }
+
+        // 2. handle included requests
+        Set<ActivityLoggingRequestProperties> loggingRequests = properties.getIncludedRequests();
+        for (ActivityLoggingRequestProperties loggingRequest : loggingRequests) {
+            result.put(loggingRequest.getName(), loggingRequest);
+        }
+
+        // 3. handle excluded category names
+        Set<ActivityLoggingCategoryName> excludedCategoryNames = properties.getExcludedCategoryNames();
+        for (ActivityLoggingCategoryName excludedCategoryName : excludedCategoryNames) {
+            Set<TurmsRequest.KindCase> excludedRequestNames = getRequestsFromCategoryName(excludedCategoryName);
+            for (TurmsRequest.KindCase excludedRequestName : excludedRequestNames) {
+                result.remove(excludedRequestName);
+            }
+        }
+
+        // 4. handle excluded request names
+        Set<TurmsRequest.KindCase> excludedRequestNames = properties.getExcludedRequestNames();
+        for (TurmsRequest.KindCase excludedRequestName : excludedRequestNames) {
+            result.remove(excludedRequestName);
+        }
+
+        return result;
+    }
+
+    private Set<ActivityLoggingRequestProperties> getRequestsFromCategory(ActivityLoggingCategoryProperties category) {
+        ActivityLoggingCategoryName categoryName = category.getName();
+        Set<TurmsRequest.KindCase> requests = getRequestsFromCategoryName(categoryName);
+        if (requests.isEmpty()) {
+            return Collections.emptySet();
+        } else {
+            Set<ActivityLoggingRequestProperties> loggingRequests = Sets.newHashSetWithExpectedSize(requests.size());
+            for (TurmsRequest.KindCase request : requests) {
+                loggingRequests.add(new ActivityLoggingRequestProperties(request, category.getSampleRate()));
+            }
+            return loggingRequests;
+        }
+    }
+
+    private Set<TurmsRequest.KindCase> getRequestsFromCategoryName(ActivityLoggingCategoryName name) {
+        switch (name) {
+            case ALL:
+                Set<TurmsRequest.KindCase> createRequests = getRequestsFromCategoryName(ActivityLoggingCategoryName.CREATE);
+                Set<TurmsRequest.KindCase> deleteRequests = getRequestsFromCategoryName(ActivityLoggingCategoryName.DELETE);
+                Set<TurmsRequest.KindCase> updateRequests = getRequestsFromCategoryName(ActivityLoggingCategoryName.UPDATE);
+                Set<TurmsRequest.KindCase> queryRequests = getRequestsFromCategoryName(ActivityLoggingCategoryName.QUERY);
+                Set<TurmsRequest.KindCase> result = Sets.newHashSetWithExpectedSize(
+                        createRequests.size()
+                                + deleteRequests.size()
+                                + updateRequests.size()
+                                + queryRequests.size());
+                result.addAll(createRequests);
+                result.addAll(deleteRequests);
+                result.addAll(updateRequests);
+                result.addAll(queryRequests);
+                return result;
+            case CREATE:
+                return Set.of(
+                        CREATE_MESSAGE_REQUEST,
+                        CREATE_FRIEND_REQUEST_REQUEST,
+                        CREATE_RELATIONSHIP_GROUP_REQUEST,
+                        CREATE_RELATIONSHIP_REQUEST,
+                        CREATE_GROUP_REQUEST,
+                        CREATE_GROUP_BLACKLISTED_USER_REQUEST,
+                        CREATE_GROUP_INVITATION_REQUEST,
+                        CREATE_GROUP_JOIN_REQUEST_REQUEST,
+                        CREATE_GROUP_JOIN_QUESTION_REQUEST,
+                        CREATE_GROUP_MEMBER_REQUEST);
+            case DELETE:
+                return Set.of(
+                        DELETE_RESOURCE_REQUEST,
+                        DELETE_RELATIONSHIP_GROUP_REQUEST,
+                        DELETE_RELATIONSHIP_REQUEST,
+                        DELETE_GROUP_REQUEST,
+                        DELETE_GROUP_BLACKLISTED_USER_REQUEST,
+                        DELETE_GROUP_INVITATION_REQUEST,
+                        DELETE_GROUP_JOIN_REQUEST_REQUEST,
+                        DELETE_GROUP_JOIN_QUESTION_REQUEST,
+                        DELETE_GROUP_MEMBER_REQUEST);
+            case UPDATE:
+                return Set.of(
+                        ACK_REQUEST,
+                        UPDATE_MESSAGE_REQUEST,
+                        UPDATE_TYPING_STATUS_REQUEST,
+                        UPDATE_USER_LOCATION_REQUEST,
+                        UPDATE_USER_ONLINE_STATUS_REQUEST,
+                        UPDATE_USER_REQUEST,
+                        UPDATE_FRIEND_REQUEST_REQUEST,
+                        UPDATE_RELATIONSHIP_GROUP_REQUEST,
+                        UPDATE_RELATIONSHIP_REQUEST,
+                        UPDATE_GROUP_REQUEST,
+                        UPDATE_GROUP_JOIN_QUESTION_REQUEST,
+                        UPDATE_GROUP_MEMBER_REQUEST);
+            case QUERY:
+                return Set.of(
+                        QUERY_SIGNED_GET_URL_REQUEST,
+                        QUERY_SIGNED_PUT_URL_REQUEST,
+                        QUERY_MESSAGE_STATUSES_REQUEST,
+                        QUERY_MESSAGES_REQUEST,
+                        QUERY_PENDING_MESSAGES_WITH_TOTAL_REQUEST,
+                        QUERY_USER_PROFILE_REQUEST,
+                        QUERY_USER_IDS_NEARBY_REQUEST,
+                        QUERY_USER_INFOS_NEARBY_REQUEST,
+                        QUERY_USER_ONLINE_STATUSES_REQUEST,
+                        QUERY_FRIEND_REQUESTS_REQUEST,
+                        QUERY_RELATED_USER_IDS_REQUEST,
+                        QUERY_RELATIONSHIP_GROUPS_REQUEST,
+                        QUERY_RELATIONSHIPS_REQUEST,
+                        QUERY_GROUP_REQUEST,
+                        QUERY_JOINED_GROUP_IDS_REQUEST,
+                        QUERY_JOINED_GROUP_INFOS_REQUEST,
+                        QUERY_GROUP_BLACKLISTED_USER_IDS_REQUEST,
+                        QUERY_GROUP_BLACKLISTED_USER_INFOS_REQUEST,
+                        QUERY_GROUP_INVITATIONS_REQUEST,
+                        QUERY_GROUP_JOIN_REQUESTS_REQUEST,
+                        QUERY_GROUP_JOIN_QUESTIONS_REQUEST,
+                        QUERY_GROUP_MEMBERS_REQUEST);
+            case STORAGE:
+                return Set.of(DELETE_RESOURCE_REQUEST,
+                        QUERY_SIGNED_GET_URL_REQUEST,
+                        QUERY_SIGNED_PUT_URL_REQUEST);
+            case MESSAGE:
+                return Set.of(CREATE_MESSAGE_REQUEST,
+                        QUERY_MESSAGE_STATUSES_REQUEST,
+                        QUERY_MESSAGES_REQUEST,
+                        QUERY_PENDING_MESSAGES_WITH_TOTAL_REQUEST,
+                        UPDATE_MESSAGE_REQUEST,
+                        UPDATE_TYPING_STATUS_REQUEST);
+            case USER:
+                return Set.of(QUERY_USER_PROFILE_REQUEST,
+                        QUERY_USER_IDS_NEARBY_REQUEST,
+                        QUERY_USER_INFOS_NEARBY_REQUEST,
+                        QUERY_USER_ONLINE_STATUSES_REQUEST,
+                        UPDATE_USER_LOCATION_REQUEST,
+                        UPDATE_USER_ONLINE_STATUS_REQUEST,
+                        UPDATE_USER_REQUEST);
+            case USER_RELATIONSHIP:
+                return Set.of(CREATE_FRIEND_REQUEST_REQUEST,
+                        CREATE_RELATIONSHIP_GROUP_REQUEST,
+                        CREATE_RELATIONSHIP_REQUEST,
+                        DELETE_RELATIONSHIP_GROUP_REQUEST,
+                        DELETE_RELATIONSHIP_REQUEST,
+                        QUERY_FRIEND_REQUESTS_REQUEST,
+                        QUERY_RELATED_USER_IDS_REQUEST,
+                        QUERY_RELATIONSHIP_GROUPS_REQUEST,
+                        QUERY_RELATIONSHIPS_REQUEST,
+                        UPDATE_FRIEND_REQUEST_REQUEST,
+                        UPDATE_RELATIONSHIP_GROUP_REQUEST,
+                        UPDATE_RELATIONSHIP_REQUEST);
+            case GROUP:
+                return Set.of(CREATE_GROUP_REQUEST,
+                        DELETE_GROUP_REQUEST,
+                        QUERY_GROUP_REQUEST,
+                        QUERY_JOINED_GROUP_IDS_REQUEST,
+                        QUERY_JOINED_GROUP_INFOS_REQUEST,
+                        UPDATE_GROUP_REQUEST);
+            case GROUP_BLACKLIST:
+                return Set.of(CREATE_GROUP_BLACKLISTED_USER_REQUEST,
+                        DELETE_GROUP_BLACKLISTED_USER_REQUEST,
+                        QUERY_GROUP_BLACKLISTED_USER_IDS_REQUEST,
+                        QUERY_GROUP_BLACKLISTED_USER_INFOS_REQUEST);
+            case GROUP_ENROLLMENT:
+                return Set.of(CHECK_GROUP_JOIN_QUESTIONS_ANSWERS_REQUEST,
+                        CREATE_GROUP_INVITATION_REQUEST,
+                        CREATE_GROUP_JOIN_REQUEST_REQUEST,
+                        CREATE_GROUP_JOIN_QUESTION_REQUEST,
+                        DELETE_GROUP_INVITATION_REQUEST,
+                        DELETE_GROUP_JOIN_REQUEST_REQUEST,
+                        DELETE_GROUP_JOIN_QUESTION_REQUEST,
+                        QUERY_GROUP_INVITATIONS_REQUEST,
+                        QUERY_GROUP_JOIN_REQUESTS_REQUEST,
+                        QUERY_GROUP_JOIN_QUESTIONS_REQUEST,
+                        UPDATE_GROUP_JOIN_QUESTION_REQUEST);
+            case GROUP_MEMBER:
+                return Set.of(CREATE_GROUP_MEMBER_REQUEST,
+                        DELETE_GROUP_MEMBER_REQUEST,
+                        QUERY_GROUP_MEMBERS_REQUEST,
+                        UPDATE_GROUP_MEMBER_REQUEST);
+            case NONE:
+                return Collections.emptySet();
+            default:
+                throw new IllegalStateException("Unexpected value: " + name);
+        }
+    }
 }
